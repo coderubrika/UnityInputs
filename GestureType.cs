cf56477d430bc0123f013e71d1e42ca956d1cfb5
Anton Samartsev <coderubrika@gmail.com>
2023-09-18 21:07:11 +0300

    initial

---
+++ GestureType.cs
@@ -0,0 +1,14 @@
+﻿namespace Suburb.Inputs
+{
+    public enum GestureType
+    {
+        None,
+        Down,
+        Hold,
+        DragStart,
+        Drag,
+        DragEnd,
+        Up,
+        Zoom
+    }
+}
