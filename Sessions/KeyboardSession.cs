48bf9dac54cdfa165bdc6f48e45df29e2885772c
Anton Samartsev <coderubrika@gmail.com>
2025-01-03 21:54:36 +0300

    improve

--- Sessions/KeyboardSession.cs
+++ Sessions/KeyboardSession.cs
@@ -4,19 +4,27 @@ using UnityEngine.InputSystem;
 
 namespace Suburb.Inputs
 {
-    public class KeyboardSession : DisposableObject
+    public class KeyboardSession : IDisposable
     {
         private readonly Func<KeyboardSession, Key, IObservable<bool>> eventsGetter;
         
+        private Action onDispose;
+        
         public KeyboardSession(Func<KeyboardSession, Key, IObservable<bool>> eventsGetter, Action onDispose)
-        : base(onDispose)
         {
+            this.onDispose = onDispose;
             this.eventsGetter = eventsGetter;
         }
 
         public IObservable<bool> OnKey(Key key)
         {
             return eventsGetter.Invoke(this, key);
+        }
+
+        public void Dispose()
+        {
+            onDispose?.Invoke();
+            onDispose = null;
         }
     }
 }
