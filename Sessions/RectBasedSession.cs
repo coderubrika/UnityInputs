9fe338e9eeb7926519efb1236aed82b1b82b5c43
Anton Samartsev <coderubrika@gmail.com>
2025-01-08 17:25:28 +0300

    add excluded rect

--- Sessions/RectBasedSession.cs
+++ Sessions/RectBasedSession.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Suburb.Utils;
+using UniRx;
 using UnityEngine;
 
 namespace Suburb.Inputs
@@ -7,12 +10,17 @@ namespace Suburb.Inputs
     public class RectBasedSession : CompositorsSession, IPointerSession
     {
         private readonly RectTransform bounds;
-        private readonly RectTransform[] excludedRects;
+        private readonly LinkedList<RectTransform> excludedRects = new();
         
-        public RectBasedSession(RectTransform bounds, RectTransform[] excludedRects = null)
+        public RectBasedSession(RectTransform bounds, RectTransform[] excludedRectsTransforms = null)
         {
             this.bounds = bounds;
-            this.excludedRects = excludedRects;
+            
+            if (excludedRectsTransforms.IsNullOrEmpty())
+                return;
+            
+            foreach (var rectTransform in excludedRectsTransforms)
+                excludedRects.AddFirst(rectTransform);
         }
         
         public bool CheckIncludeInBounds(Vector2 point)
@@ -25,5 +33,11 @@ namespace Suburb.Inputs
         
         public Vector2 TransformVector(Vector2 delta) => bounds.TransformVector(delta);
         public Vector2 TransformPoint(Vector2 position) => bounds.TransformPoint(position);
+
+        public IDisposable AddExcludedRect(RectTransform rect)
+        {
+            var node = excludedRects.AddFirst(rect);
+            return Disposable.Create(() => excludedRects.Remove(node));
+        }
     }
 }
